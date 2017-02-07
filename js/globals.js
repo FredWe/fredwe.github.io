45b6ac6ad2f7ae73012a0f9cd7087d8215466d1e
FredWe <fredw.mail@gmail.com>
2017-02-07 09:59:08 +0100

    first minimized ver.

--- js/globals.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var USERNAME = 'fredwe', // github username
-    REPO = 'issue-blog' // github repository
-    //TOKEN = '70f3de02cf500ec91b2f7eea4daef5002580c5a3'; // github app access token
